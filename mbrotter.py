82e746a00486a8c2aa288418acf83257b26bd27d
Iamaprogramador <104740094+Iamaprogramador@users.noreply.github.com>
2023-03-09 14:24:42 -0700

    Partially convert to python compiler

--- mbrotter.py
+++ mbrotter.py
@@ -1,43 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#!/usr/bin/python
+import os
 
-void mndl(double WIDTH, double HEIGHT, double XCENTER, double YCENTER, double DELTAPIX, double max_loop, char* filename) {
-    double x, y, x1, y1, x2, y2;
-    double max=log(max_loop);
-    double loop;
-    FILE *fp;
-    fp = fopen(filename, "wb");
-    fprintf(fp, "P6\n%d %d\n255\n", (int)WIDTH, (int)HEIGHT);
-    for (int yy = 0; yy < HEIGHT; yy++) {
-        y = YCENTER + ((yy - HEIGHT / 2) * DELTAPIX); 
-	for (int xx = 0; xx < WIDTH; xx++) {
-            x = XCENTER + ((xx - WIDTH / 2) * DELTAPIX);
-	    loop = 0;
-	    if ((x + 1)*(x + 1) + y*y <= .0625) {
-		    loop = max_loop;
+f = open('mbrotter.c','w')
+#standard headers
+f.write('#include <stdio.h>\n#include <stdlib.h>\n#include <math.h>')
+#main loop
+f.write('void mndl(double WIDTH, double HEIGHT, double XCENTER, double YCENTER, double DELTAPIX, double maxit, char* filename) {')
+#var declarations
+f.write('\ndouble x, y, x1, y1, x2, y2, max=log(maxit), i;')
+f.write('\nFILE *fp;')
+#file stuff
+f.write('fp = fopen(filename, "wb");\nfprintf(fp, "P6\n%d %d\n255\n", (int)WIDTH, (int)HEIGHT);')
+    for (int yloc = 0; yloc < HEIGHT; yloc++) {
+        cy = YCENTER + ((yloc - HEIGHT / 2) * DELTAPIX); 
+	for (int xloc = 0; xloc < WIDTH; xloc++) {
+            cx = XCENTER + ((xloc - WIDTH / 2) * DELTAPIX);
+	    i = 0;
+	    if ((cx + 1)*(cx + 1) + cy*cy <= .0625) {
+		    i = maxit;
 	    }
-	    double q = (x-.25) * (x-.25) + y*y;
-		if (q*(q+(x-.25)) <= .25*y*y) {
-			loop = max_loop;
+	    double q = (cx-.25) * (cx-.25) + cy*cy;
+		if (q*(q+(cx-.25)) <= .25*cy*cy) {
+			i = maxit;
 		}
-            x1 = 0;
-            y1 = 0;
+            x = 0;
+            y = 0;
             x2 = 0;
 	    y2 = 0;
-            while (loop < max_loop && x2 + y2 <= 40) {
-                y1 = 2 * x1 * y1 + y;
-                x1 = x2 - y2 + x;
-		x2 = x1 * x1;
-		y2 = y1 * y1;
-                loop++;
+            while (i < maxit && x2 + y2 <= 40) {
+                y = 2 * x * y + cy;
+                x = x2 - y2 + cx;
+		x2 = x * x;
+		y2 = y * y;
+                i++;
             }
-            if (loop == max_loop) {
+            if (i == maxit) {
                 fputc(0, fp);
                 fputc(0, fp);
                 fputc(0, fp);
             } else {
-		loop=256*(log(loop)/max);
+		i=256*(log(i)/max);
                 fputc((char)sin(1.25*loop), fp);
                 fputc((char)sin(3.78*loop), fp);
                 fputc((char)sin(7.62*loop), fp);
@@ -72,3 +74,5 @@ int main(int argc, char* argv[]) {
     }
     return 0;
 }
+
+f.close()
