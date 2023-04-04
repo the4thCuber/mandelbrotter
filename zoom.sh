3472717a429ae9e5f9e5e94e9a4471df1b7eddf2
Iamaprogramador <104740094+Iamaprogramador@users.noreply.github.com>
2023-04-04 09:25:14 -0700

    Updated from local via upload
    I've been working on the project on my local machine, but haven't been pushing the commits back to this repository. The local version is now so far upstream that I find it necessary to just upload the latest version.

--- zoom.sh
+++ zoom.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+start=$EPOCHREALTIME
+
 # Parameters for the Mandelbrot set image
-width=720
-height=1560
-max_loop=150000
+width=3840
+height=2160
+maxit=10000
 
 # Parameters for the zoom sequence
-xcenter=-.442928
-ycenter=.567027
-num_frames=1500
+xcenter=-0.74757647291674
+ycenter=-0.08137850467218
+num_frames=7000
 
 #deltapix math stuff
-zoom_i=-6
-zoom_f=1
+zoom_i=-2.5
+zoom_f=-15.58
 
 # Create a directory to store the images
 mkdir images
 
 # Generate the images for the zoom sequence
 gcc -o mandelbrot mandelbrot.c -lm
-./mandelbrot $width $height $xcenter $ycenter $zoom_i $zoom_f $num_frames $max_loop
+./mandelbrot $width $height $xcenter $ycenter $zoom_i $zoom_f $num_frames $maxit
 
 # Use ffmpeg to turn the images into a video
-ffmpeg -f image2 -framerate 30 -i images/%04d.ppm -s $width x$height seoing.avi
+ffmpeg -f image2 -framerate 30 -i images/%04d.ppm -s  odnwepodhr.avi
 
 # Clean up the images
 #rm -r images
+
+echo "$EPOCHREALTIME-$start" | "bc -l" >> data.txt
